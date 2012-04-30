783180e826bbce1bd43b7198a357dd0cc3675fa6
David Rice <me@davidjrice.co.uk>
2012-04-30 02:50:34 +0100

    gem install couchbase

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -53,6 +53,7 @@ class LanguagePack::Ruby < LanguagePack::Base
     allow_git do
       install_libvbucket
       install_libcouchbase
+      run("gem install couchbase")
 
       install_language_pack_gems
       build_bundler
